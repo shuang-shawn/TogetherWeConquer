9b71eb4c42a18691486827ce887c22ebb7cd45d5
Robert Wilson <robertwilson875@gmail.com>
2024-10-08 11:21:21 -0700

    cleaned up player movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController1 : MonoBehaviour
+public class PlayerMovement:MonoBehaviour
 {
     //create private internal references
     private InputActions inputActions;
-    private InputAction movementP1;
+    private InputAction movement;
 
     Rigidbody rb;
 
@@ -21,22 +21,30 @@ public class PlayerController1 : MonoBehaviour
     //called when script enabled
     private void OnEnable()
     {
-        movementP1 = inputActions.Player1.MovementP1; //get reference to movement action
-        movementP1.Enable();
+        if (gameObject.name.Equals("Player1"))
+        {
+            movement = inputActions.Player1.MovementP1; //get reference to movement action
+        }
+        else if (gameObject.name.Equals("Player2"))
+            {
+            movement = inputActions.Player2.MovementP2; //get reference to movement action
+        }
+        movement.Enable();
+
 
     }
 
     //called when script disabled
     private void OnDisable()
     {
-        movementP1.Disable();
+        movement.Disable();
 
     }
 
     //called every physics update
     private void FixedUpdate()
     {
-        Vector2 v2P1 = movementP1.ReadValue<Vector2>(); //extract 2d input data
+        Vector2 v2P1 = movement.ReadValue<Vector2>(); //extract 2d input data
         Vector3 v3P1 = new Vector3(v2P1.x, 0, v2P1.y); //convert to 3d space
 
         //transform.Translate(v3); //moves transform, ignoring physics
