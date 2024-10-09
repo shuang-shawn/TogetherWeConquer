072666a4432a48d204b9ae2000a04b3c4728cc22
Shuang Wu <shuang111994@gmail.com>
2024-10-09 16:51:00 -0700

    implemented hd2d with player movement, deleted movement momentum

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement:MonoBehaviour
     //create private internal references
     private InputActions inputActions;
     private InputAction movement;
+    public float movementSpeed = 5f;
+    public int playerNo = 1;
 
     Rigidbody rb;
 
@@ -21,14 +23,15 @@ public class PlayerMovement:MonoBehaviour
     //called when script enabled
     private void OnEnable()
     {
-        if (gameObject.name.Equals("Player1"))
+        if (playerNo == 1)
         {
-            movement = inputActions.Player1.MovementP1; //get reference to movement action
+            movement = inputActions.Player1.Movement; //get reference to movement action
         }
-        else if (gameObject.name.Equals("Player2"))
+        else if (playerNo == 2)
             {
-            movement = inputActions.Player2.MovementP2; //get reference to movement action
+            movement = inputActions.Player2.Movement; //get reference to movement action
         }
+
         movement.Enable();
 
 
@@ -48,7 +51,8 @@ public class PlayerMovement:MonoBehaviour
         Vector3 v3P1 = new Vector3(v2P1.x, 0, v2P1.y); //convert to 3d space
 
         //transform.Translate(v3); //moves transform, ignoring physics
-        rb.AddForce(v3P1, ForceMode.VelocityChange); //apply instant physics force, ignoring mass
+        rb.velocity = v3P1 *movementSpeed;
+        // rb.AddForce(v3P1, ForceMode.VelocityChange); //apply instant physics force, ignoring mass
 
     }
 }
