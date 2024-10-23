1d2754518169a71b29f65e14f967dc4d483af2cb
RichardOhata <richardohata@gmail.com>
2024-10-23 00:00:26 -0700

    Adjusted scene transitions for title scene

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,4 +9,9 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }   
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
