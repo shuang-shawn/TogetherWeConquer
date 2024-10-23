9c647750df503846e54b77e34df92aaf05021625
Shuang Wu <shuang111994@gmail.com>
2024-10-23 15:05:09 -0700

    implemented skill collision handler and enemy take damage method

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public int damageAmount = 0;
+    public int maxHealth = 100;
+    public int maxBossHealth = 250;
+    public int currentHealth = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         if (gameObject.tag == "boss"){
             damageAmount = 25;
+            currentHealth = maxBossHealth;
         } else if (gameObject.tag == "mob") {
             damageAmount = 10;
+            currentHealth = maxHealth;
         }
     }
 
@@ -20,5 +25,24 @@ public class EnemyManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+        Debug.Log(gameObject.name + " took " + damage + " damage!");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log(gameObject.name + " has died!");
+        // Add death handling here (destroy, play animation, etc.)
+        Destroy(gameObject);
     }
 }
