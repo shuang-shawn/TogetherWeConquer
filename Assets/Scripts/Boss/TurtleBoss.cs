342859a2734d157265d8be328dbc7c7e3bae79ce
Shuang Wu <shuang111994@gmail.com>
2024-11-06 23:04:07 -0800

    implemented turtle boss prefab, animation transition logic

---
+++ Assets/Scripts/Boss/TurtleBoss.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleBoss : MonoBehaviour
+{
+    [Header("Rage Settings")]
+    public float rageDuration = 5f; // Duration in seconds for the rage state
+    public float chillDuration = 5f;
+    private Animator animator;
+    private bool isSpikeOut = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(SpikeCycle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        // Coroutine to handle the rage state cycle
+    private IEnumerator SpikeCycle()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(chillDuration); // Optional delay before entering rage mode
+
+            EnterSpikeMode();
+
+            yield return new WaitForSeconds(rageDuration); // Wait for the rage duration
+
+            ExitSpikeMode();
+
+            
+        }
+    }
+
+    private void EnterSpikeMode()
+    {
+        animator.SetTrigger("spike_out");
+        isSpikeOut = true;
+        Debug.Log("Boss has entered spike mode!");
+    }
+
+    private void ExitSpikeMode()
+    {
+        animator.SetTrigger("spike_in");
+        isSpikeOut = false;
+        Debug.Log("Boss has exited spike mode.");
+    }
+}
