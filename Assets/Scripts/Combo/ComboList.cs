2a25f6ff0faa5d713053b5537f0af9aa076b5e8f
Shuang Wu <shuang111994@gmail.com>
2024-11-14 10:30:23 -0800

    added dash icon

--- Assets/Scripts/Combo/ComboList.cs
+++ Assets/Scripts/Combo/ComboList.cs
@@ -15,7 +15,7 @@ public class ComboList : MonoBehaviour
         // Load sprites here instead of in the constructor
         soloComboList = new List<Combo>()
         {
-            new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.RightArrow }, "dash"), 
+            new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.RightArrow }, "dash", Resources.Load<Sprite>("Skill Icons/dash")), 
             new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.UpArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.RightArrow }, "shadow", Resources.Load<Sprite>("Skill Icons/shadow")),
             new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow }, "stone", Resources.Load<Sprite>("Skill Icons/stone")),
             new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.LeftArrow, KeyCode.LeftArrow, KeyCode.LeftArrow, KeyCode.LeftArrow }, "dash"),
