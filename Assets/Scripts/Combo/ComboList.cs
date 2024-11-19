76b76928442d225c84814d8198c3d7caa9851d06
Jerry Han <jerry000@hotmail.ca>
2024-11-18 19:24:12 -0800

    fixed errors and updated to use new combo ui system

--- Assets/Scripts/Combo/ComboList.cs
+++ Assets/Scripts/Combo/ComboList.cs
@@ -29,7 +29,7 @@ public class ComboList : MonoBehaviour
             new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.LeftArrow, KeyCode.LeftArrow, KeyCode.LeftArrow, KeyCode.LeftArrow }, "dash"),
             new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.DownArrow }, "helpinghand", Resources.Load<Sprite>("Skill Icons/InvisSkillIcon")),
             new Combo(ComboType.Solo, new List<KeyCode> { KeyCode.RightArrow, KeyCode.RightArrow, KeyCode.RightArrow, KeyCode.RightArrow }, "iceground", Resources.Load<Sprite>("Skill Icons/ice")),
-            new Combo(ComboType.Solo, new List<KeyCode> {KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.DownArrow}, "swap")
+            new Combo(ComboType.Solo, new List<KeyCode> {KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.DownArrow}, "swap")
         };
 
         duoComboList = new List<Combo>()
