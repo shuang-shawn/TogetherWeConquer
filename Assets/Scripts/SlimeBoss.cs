fa811100fd8a8f18d4a82d4d5204e55646715f2b
Robert Wilson <robertwilson875@gmail.com>
2024-11-20 20:50:01 -0800

    got portals working, still some glitches tho, need to make dire area and make snow stay on ground

--- Assets/Scripts/SlimeBoss.cs
+++ Assets/Scripts/SlimeBoss.cs
@@ -1,11 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SlimeBoss : MonoBehaviour
 {
-
     private Vector3 startPosition;
     private GameObject closestPlayerObj = null;
     public float speed = 2.0f;
@@ -18,7 +15,7 @@ public class SlimeBoss : MonoBehaviour
     private float jumpAttackHeight = 10f;
     float jumpAttackLandingTimer = 0f;
     public GameObject shadow;
-    private Vector3 landingPoint = new Vector3(0,0,0);
+    private Vector3 landingPoint = new Vector3(0, 0, 0);
     private Coroutine stopwatchCoroutine;
     public int stayInAir = 5;
     public int damage = 20;
@@ -29,87 +26,95 @@ public class SlimeBoss : MonoBehaviour
     public float specialAttackInterval = 4f;
     public bool IsDead = false;
 
-    //FOR COLLISION STUFF TO BE IMPLEMENTED LATER
-    //CURRENT IDEA FORMAT
-
-    public void handleCollision(GameObject gameObject){
-        
-        // Enemy gets hit by a combo
-        if(gameObject.tag == "TestCombo"){
+    public void handleCollision(GameObject gameObject)
+    {
+        if (gameObject.tag == "TestCombo")
+        {
             string hitMessage = "Hit by Combo";
             Debug.Log(hitMessage);
         }
-
-        // Enemy hits player
-        else if(gameObject.tag == "Player1"){
+        else if (gameObject.tag == "Player1")
+        {
             gameObject.GetComponent<PlayerStats>().playerGotHit(damage);
-
-            //Add gets knocked back a certain amount
         }
     }
- 
 
-    //Maybe put these two functions inside a class that is inherited by enemies?
-    
-    private void findClosestPlayer(){
+    private void findClosestPlayer()
+    {
         GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
         GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
         float player1Distance = 9999f;
         float player2Distance = 9999f;
-        if(player1 != null) {
 
+        if (player1 != null && player1.activeInHierarchy)
+        {
             player1Distance = findDistance(player1);
         }
-        if(player2 != null) {
-
+        if (player2 != null && player2.activeInHierarchy)
+        {
             player2Distance = findDistance(player2);
         }
 
-        if(player2Distance > player1Distance){
+        if (player1 != null && player1.activeInHierarchy && (player2 == null || !player2.activeInHierarchy || player1Distance <= player2Distance))
+        {
             closestPlayerObj = player1;
-        } else if (player2Distance < player1Distance) {
+        }
+        else if (player2 != null && player2.activeInHierarchy)
+        {
             closestPlayerObj = player2;
-        } else {
+        }
+        else
+        {
             closestPlayerObj = null;
         }
 
+        if (closestPlayerObj == null)
+        {
+            Debug.LogWarning("No player found. closestPlayerObj is null.");
+        }
     }
 
-    // Returns the distance between the object the script is attached to, and the targetObject
-    // Takes in a single GameObject and returns a float representing the distance
-    private float findDistance(GameObject targetObject){
+    private float findDistance(GameObject targetObject)
+    {
         return Vector3.Distance(transform.position, targetObject.transform.position);
     }
 
-    private void controlHopping(){
+    private void controlHopping()
+    {
         hopMotion = Mathf.Sin(Time.time * hopFrequency) * hopHeight;
     }
 
-
-    //Timer coroutine stuf
-    private void StartStopwatch(){
-        if(stopwatchCoroutine == null) {
+    private void StartStopwatch()
+    {
+        if (stopwatchCoroutine == null)
+        {
             stopwatchCoroutine = StartCoroutine(StopwatchCoroutine());
         }
     }
 
-    private void StopStopwatch(){
-        if(stopwatchCoroutine != null) {
+    private void StopStopwatch()
+    {
+        if (stopwatchCoroutine != null)
+        {
             StopCoroutine(stopwatchCoroutine);
             stopwatchCoroutine = null;
             jumpAttackLandingTimer = 0;
         }
     }
-    
-    private IEnumerator StopwatchCoroutine() {
-        while (true) {
+
+    private IEnumerator StopwatchCoroutine()
+    {
+        while (true)
+        {
             jumpAttackLandingTimer += Time.deltaTime;
             yield return null;
         }
     }
 
-    private IEnumerator MoveToPosition(Vector3 targetPoint, float moveSpeed){
-        while (Vector3.Distance(transform.position, targetPoint) > 0.1f) {
+    private IEnumerator MoveToPosition(Vector3 targetPoint, float moveSpeed)
+    {
+        while (Vector3.Distance(transform.position, targetPoint) > 0.1f)
+        {
             transform.position = Vector3.MoveTowards(transform.position, targetPoint, moveSpeed * Time.deltaTime);
             yield return null;
         }
@@ -117,74 +122,76 @@ public class SlimeBoss : MonoBehaviour
         transform.position = targetPoint;
     }
 
-    private IEnumerator TrackPlayer(){
-        // Debug.Log("TrackPlayer started");
-        // Debug.Log("Stopwatch value" + jumpAttackLandingTimer);
-        StartStopwatch(); 
-        while(jumpAttackLandingTimer < stayInAir) {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector3(closestPlayerObj.transform.position.x, jumpAttackHeight, closestPlayerObj.transform.position.z), speed * Time.deltaTime *2);
+    private IEnumerator TrackPlayer()
+    {
+        StartStopwatch();
+        while (jumpAttackLandingTimer < stayInAir)
+        {
+            if (closestPlayerObj != null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, new Vector3(closestPlayerObj.transform.position.x, jumpAttackHeight, closestPlayerObj.transform.position.z), speed * Time.deltaTime * 2);
+            }
+            else
+            {
+                Debug.LogWarning("closestPlayerObj is null. Stopping TrackPlayer coroutine.");
+                StopStopwatch();
+                yield break;
+            }
             yield return null;
         }
-        
-        // Debug.Log("5 seconds passed");
         StopStopwatch();
-        
-        // yield return null;
     }
 
-
-    private IEnumerator JumpAttackSequence(){
-        //jump into air
+    private IEnumerator JumpAttackSequence()
+    {
         yield return StartCoroutine(MoveToPosition(new Vector3(transform.position.x, jumpAttackHeight, transform.position.z), jumpSpeed));
         GameObject instantiatedShadow = Instantiate(shadow, new Vector3(transform.position.x, 0.1f, transform.position.z), Quaternion.identity);
 
-        //Track closest player for 5 seconds
         yield return StartCoroutine(TrackPlayer());
-        //Land
+
         yield return StartCoroutine(MoveToPosition(new Vector3(transform.position.x, startPosition.y, transform.position.z), dropSpeed));
         Destroy(instantiatedShadow);
-        //Daze effect
+
         yield return new WaitForSeconds(landingDelay);
         jumpAttacking = false;
         timePassed = 0f;
-
-        
     }
 
-    private void revisedJumpAttack(){
+    private void revisedJumpAttack()
+    {
         StartCoroutine(JumpAttackSequence());
     }
 
-    private void HopToPlayer(){
-        if (closestPlayerObj == null) {
+    private void HopToPlayer()
+    {
+        if (closestPlayerObj == null)
+        {
             return;
         }
         Vector3 playerPosition = new Vector3(closestPlayerObj.transform.position.x, startPosition.y, closestPlayerObj.transform.position.z);
 
-        // While the sine value is greater than 0, move character
-        // Simulates jump, pause, jump
-        if(hopMotion > 0){
-            //move enemy
-
-            // Move towards the player
+        if (hopMotion > 0)
+        {
             transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
-
-            // Simulate the hopping motion
             transform.position = new Vector3(transform.position.x, startPosition.y + hopMotion, transform.position.z);
         }
     }
+
     void Start()
     {
         startPosition = transform.position;
     }
 
-    private void updateSpeed(float percent){
+    private void updateSpeed(float percent)
+    {
         speed *= percent;
         hopFrequency *= percent;
         jumpSpeed *= percent;
         dropSpeed *= percent;
     }
-    void ResetSpeed() {
+
+    void ResetSpeed()
+    {
         speed = 2.0f;
         jumpSpeed = 20f;
         dropSpeed = 20f;
@@ -193,46 +200,47 @@ public class SlimeBoss : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (!IsDead) {
+        if (!IsDead)
+        {
             controlHopping();
 
             timePassed += Time.deltaTime;
-            if (previousSpeedPercent != speedPercent) {
-                if (speedPercent == 1) {
+            if (previousSpeedPercent != speedPercent)
+            {
+                if (speedPercent == 1)
+                {
                     ResetSpeed();
-                } else {
+                }
+                else
+                {
                     ResetSpeed();
                     updateSpeed(speedPercent);
                 }
                 previousSpeedPercent = speedPercent;
             }
-            
-            findClosestPlayer();
-            
-            if(timePassed >= specialAttackInterval && !jumpAttacking && hopMotion < -0.9f){
-                jumpAttacking = true;
 
-                // Debug.Log("Space pressed");
-                // Debug.Log(jumpAttacking);
-                // jumpAttack();
+            findClosestPlayer();
+
+            if (timePassed >= specialAttackInterval && !jumpAttacking && hopMotion < -0.9f)
+            {
+                jumpAttacking = true;
                 revisedJumpAttack();
             }
 
-            if(!jumpAttacking) {
+            if (!jumpAttacking)
+            {
                 HopToPlayer();
             }
-
-            // if(jumpAttacking){
-            //     jumpAttack();
-            // } else {
-            //     HopToPlayer();
-            // }
-
         }
-        
-        
-        
+    }
 
+    public void Pause()
+    {
+        enabled = false;
+    }
 
+    public void Resume()
+    {
+        enabled = true;
     }
 }
